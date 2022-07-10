bfbb8fc7234e62f294585532a5b55d1aa6df71e9
Hayden Moritz <Moritz.Hayden@gmail.com>
2022-07-10 14:28:34 -0500

    Dynamize bio

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,24 @@
+function render() {
+  bio();
+  footer();
+
+  window.addEventListener("resize", render, true);
+}
+
+function bio() {
+  var width = window.innerWidth;
+  var bio =
+    "<p><span>👨‍💻 Software Engineering |</span><span>🔐 Cybersecurity |</span><span>🔨 Hackathons</span></p>";
+
+  if (width < 650) {
+    bio =
+      "<p><span>👨‍💻 Software Engineering</span><br><span>🔐 Cybersecurity</span><br><span>🔨 Hackathons</span></p>";
+  }
+
+  document.getElementById("bio_div").innerHTML = bio;
+}
+
 function footer() {
-  var i = 1;
-  var j = 1;
   document.getElementById("footer_div").innerHTML =
     "<p>Copyright &copy; " + /\d{4}/.exec(Date())[0] + " Hayden Moritz</p>";
 }
