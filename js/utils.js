c7accf99cdd2396ff94e5389a4c5d83d3162caf7
Hayden Moritz <Moritz.Hayden@gmail.com>
2022-08-10 17:52:23 -0500

    Dynamize email and phone buttons

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,7 @@
 function render() {
   bio();
   footer();
+  contact();
 
   window.addEventListener("resize", render, true);
 }
@@ -21,6 +22,17 @@ function bio() {
 function footer() {
   document.getElementById("footer_div").innerHTML =
     "<p>Copyright &copy; " + /\d{4}/.exec(Date())[0] + " Hayden Moritz</p>";
+}
+
+function contact() {
+  var width = window.innerWidth;
+  if (width < 400) {
+    document.getElementById("email_btn").innerHTML = "<img class=\"icon\" src=\"images/icons/email.svg\" alt=\"Email Icon\"/>Email Me";
+    document.getElementById("call_btn").innerHTML = "<img class=\"icon\" src=\"images/icons/phone.svg\" alt=\"Phone Icon\"/>Call Me";
+  } else {
+    document.getElementById("email_btn").innerHTML = "<img class=\"icon\" src=\"images/icons/email.svg\" alt=\"Email Icon\"/>me@haydenmoritz.com";
+    document.getElementById("call_btn").innerHTML = "<img class=\"icon\" src=\"images/icons/phone.svg\" alt=\"Phone Icon\"/>(615) 455-1656";
+  }
 }
 
 function toggleTheme() {
